eb6c081fd359df2325831e873de59ff6f17943f0
mugeshwaran64 <mugeshwaran64@gmail.com>
2026-02-03 14:58:20 +0530

    updated admin

--- backend/backend/create_superuser.py
+++ backend/backend/create_superuser.py
@@ -7,14 +7,19 @@ django.setup()
 
 from django.contrib.auth.models import User
 
-# Configuration for the Superuser
+# Configuration
 USERNAME = "admin"
 EMAIL = "admin@gmail.com"
-PASSWORD = "admin"  # <--- You can change this later in the dashboard
+PASSWORD = "admin"  # <--- Neenga keta password
 
-if not User.objects.filter(username=USERNAME).exists():
+# Check if user exists
+if User.objects.filter(username=USERNAME).exists():
+    print(f"User {USERNAME} found. Reseting password...")
+    user = User.objects.get(username=USERNAME)
+    user.set_password(PASSWORD)
+    user.save()
+    print("Password updated successfully!")
+else:
     print(f"Creating superuser: {USERNAME}...")
     User.objects.create_superuser(USERNAME, EMAIL, PASSWORD)
     print("Superuser created successfully!")
-else:
-    print("Superuser already exists. Skipping creation.")
